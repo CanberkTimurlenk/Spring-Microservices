ed92e523aa9ebebaf5542288fb88c9c707c2bdb4
Canberk TIMURLENK <canberk.timurlenk@gmail.com>
2024-04-23 23:01:43 +0300

    Add Unit Tests

--- OrderService/src/test/java/com/robotdreams/orderservice/unittest/OrderServiceUnitTests.java
+++ OrderService/src/test/java/com/robotdreams/orderservice/unittest/OrderServiceUnitTests.java
@@ -11,8 +11,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 public class OrderServiceUnitTests {
