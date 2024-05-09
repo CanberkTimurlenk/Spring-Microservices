e87d8dcdf84025cd1324a6853e90958a8bd8cba8
Canberk TIMURLENK <canberk.timurlenk@gmail.com>
2024-05-09 22:06:16 +0300

    Add Circuit Breaker For ProductFeignClient

--- OrderService/src/main/java/com/robotdreams/orderservice/exceptionHandling/ExceptionHandling.java
+++ OrderService/src/main/java/com/robotdreams/orderservice/exceptionHandling/ExceptionHandling.java
@@ -9,7 +9,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ExceptionHandling {
 
     @ExceptionHandler(UserException.class)
-    public ResponseEntity<String> businessExceptionHandler(UserException userException) {
+    public ResponseEntity<String> userExceptionHandler(UserException userException) {
         return new ResponseEntity<>(userException.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(ProductException.class)
+    public ResponseEntity<String> orderExceptionHandler(ProductException productException) {
+        return new ResponseEntity<>(productException.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
--- OrderService/src/main/java/com/robotdreams/orderservice/feign/UserFeignClient.java
+++ OrderService/src/main/java/com/robotdreams/orderservice/feign/UserFeignClient.java
@@ -6,18 +6,16 @@ import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.Date;
 
 @FeignClient(name = "userService")
 public interface UserFeignClient {
 
     @GetMapping("/users/{userId}")
-    @CircuitBreaker(name = "getInfoCBreaker", fallbackMethod = "getInfoServiceFallback")
-    UserInfoResponseDto getInfo(@PathVariable long userId);
+    @CircuitBreaker(name = "getUserInfoCBreaker", fallbackMethod = "getUserInfoServiceFallback")
+    UserInfoResponseDto getInfo(@PathVariable Long userId);
 
-    default UserInfoResponseDto getInfoServiceFallback(Long userId, Exception e) {
+    default UserInfoResponseDto getUserInfoServiceFallback(Long userId, Exception e) {
+        // Execute If UserService Is Not Accessible
         throw new UserException("An error occurred while retrieving user information." + userId);
     }
 }
