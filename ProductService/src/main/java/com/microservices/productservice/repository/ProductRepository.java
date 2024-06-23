3efa0c1266b9f040c7d6bdd1e87f1a0c6473e79f
CanberkTimurlenk <canberk.timurlenk@gmail.com>
2024-06-24 01:56:07 +0300

    Update Dockerfiles

--- ProductService/src/main/java/com/microservices/productservice/repository/ProductRepository.java
+++ ProductService/src/main/java/com/microservices/productservice/repository/ProductRepository.java
@@ -2,6 +2,7 @@ package com.microservices.productservice.repository;
 
 
 import com.microservices.productservice.entity.Product;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,9 +10,12 @@ import java.util.List;
 
 @Repository
 public interface ProductRepository extends ListCrudRepository<Product, Long> {
+
+    @EntityGraph(attributePaths = {"productDetail"})
+    List<Product> findAll();
+
+    @EntityGraph(attributePaths = {"productDetail"})
     List<Product> findByIdIn(List<Long> productIds);
 
     List<Product> findByCategory(String category);
-
-
 }
